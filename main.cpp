a49cda773a6e9bfe6555129fe752c8fd16d3af26
Ece Buse Bozkurt <250229045@kocaeli.edut.tr>
2026-05-06 22:31:25 +0300

    oyuncu hareket sistemi eklendi

--- main.cpp
+++ main.cpp
@@ -1,21 +1,36 @@
 #include <SFML/Graphics.hpp>
 int main()
-{ sf::RenderWindow window(sf::VideoMode({800,600}),"ilk oyun pencerem");
-  sf::CircleShape shape(50.f);
-  shape.setFillColor(sf::Color::Green);
-  while(window.isOpen()){
-    
-    while(const std::optional event=window.pollEvent())
-    {
-     if(event->is<sf::Event::Closed>()){
-       window.close();
-     }
-     window.clear();
-     window.draw(shape); 
-     window.display();
-    }
+{ sf::RenderWindow window(sf::VideoMode({800,600}),"platform oyunu");
+window.setFramerateLimit(60);
+//oyuncu
+  sf::RectangleShape player({50.f,70.f});
+  player.setFillColor(sf::Color::Green);
+  player.setPosition({100.f,50.f});
+  float speed=5.f;
+while(window.isOpen())
+{
+ while(const std::optional event=window.pollEvent())
+ {
+  if(event->is<sf::Event::Closed>())
+  {
+   window.close();
   }
+ }
+//sol hareket icin
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+{
+ player.move({-speed,0.f});
+}
+//sag hareket icin
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+{
+ player.move({speed,0.f});
+}
+window.clear();
+window.draw(player);
+window.display();
 
+}
 
 
 
