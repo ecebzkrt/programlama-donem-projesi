48e5e40c9f735c655fa544f09725550e3841d7ad
Ece Buse Bozkurt <250229045@kocaeli.edut.tr>
2026-05-12 16:41:01 +0300

    yer cekimi ve ziplama mekanigi eklendi.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@ int main()
 { sf::RenderWindow window(sf::VideoMode({800,600}),"platform oyunu");
 window.setFramerateLimit(60);
 //oyuncu
+float speed=5.f;
+float velocityY=0.f;
+float gravity=0.5f;
+float jumpPower=-12.f;
+bool isOnGround=false;
+sf::RectangleShape ground({800.f,50.f});
+ground.setFillColor(sf::Color::Blue);
+ground.setPosition({0.f,550.f});
   sf::RectangleShape player({50.f,70.f});
   player.setFillColor(sf::Color::Green);
   player.setPosition({100.f,50.f});
-  float speed=5.f;
+
 while(window.isOpen())
 {
  while(const std::optional event=window.pollEvent())
@@ -26,8 +34,22 @@ if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 {
  player.move({speed,0.f});
 }
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)&&isOnGround)
+{
+velocityY=jumpPower;
+isOnGround=false;
+}
+velocityY+=gravity;
+player.move({0.f,velocityY});
+if(player.getGlobalBounds().findIntersection(ground.getGlobalBounds()))
+{
+player.setPosition({player.getPosition().x,ground.getPosition().y-player.getSize().y});
+velocityY=0.f;
+isOnGround=true;
+}
 window.clear();
 window.draw(player);
+window.draw(ground);
 window.display();
 
 }
