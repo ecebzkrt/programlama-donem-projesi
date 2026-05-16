2169e64c58b77ad5e0921ae93401b37ab1891664
Ece Buse Bozkurt <250229045@kocaeli.edut.tr>
2026-05-16 23:34:20 +0300

    platform sistemi ve coin eklendi.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 int main()
 { sf::RenderWindow window(sf::VideoMode({800,600}),"platform oyunu");
 window.setFramerateLimit(60);
-//oyuncu
 float speed=5.f;
 float velocityY=0.f;
 float gravity=0.5f;
@@ -11,9 +11,27 @@ bool isOnGround=false;
 sf::RectangleShape ground({800.f,50.f});
 ground.setFillColor(sf::Color::Blue);
 ground.setPosition({0.f,550.f});
+std::vector<sf::RectangleShape>platforms;
+platforms.push_back(ground);
+sf::RectangleShape platform1({30.f,200.f});
+platform1.setFillColor(sf::Color::Red);
+platform1.setPosition({250.f,400.f});
+platforms.push_back(platform1);
+
+sf::RectangleShape platform2({30.f,200.f});
+platform2.setFillColor(sf::Color::Magenta);
+platform2.setPosition({500.f,300.f});
+platforms.push_back(platform2);
+
+//oyuncu
   sf::RectangleShape player({50.f,70.f});
   player.setFillColor(sf::Color::Green);
   player.setPosition({100.f,50.f});
+  //coin
+  sf::CircleShape coin(15.f);
+  coin.setFillColor(sf::Color::Yellow);
+  coin.setPosition({560.f,260.f});
+  bool coinCollected=false;
 
 while(window.isOpen())
 {
@@ -50,15 +68,33 @@ if(player.getPosition().x+player.getSize().x>800)
  player.setPosition({800.f-player.getSize().x,player.getPosition().y});
 }
 
-if(player.getGlobalBounds().findIntersection(ground.getGlobalBounds()))
+isOnGround=false;
+for(auto& plaform:platforms)
 {
-player.setPosition({player.getPosition().x,ground.getPosition().y-player.getSize().y});
+if(player.getGlobalBounds().findIntersection(plaform.getGlobalBounds())&&velocityY>0)
+{
+player.setPosition({
+
+player.getPosition().x,plaform.getPosition().y-player.getSize().y});
 velocityY=0.f;
 isOnGround=true;
 }
+}
+if(!coinCollected&&player.getGlobalBounds().findIntersection(coin.getGlobalBounds()))
+{
+coinCollected=true;
+}
+
 window.clear();
+for(auto&platform:platforms)
+{
+ window.draw(platform);
+}
+if(!coinCollected)
+{
+window.draw(coin);
+}
 window.draw(player);
-window.draw(ground);
 window.display();
 
 }
