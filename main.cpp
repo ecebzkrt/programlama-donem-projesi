d5b982c7a6d6a9e52ad5fe0c0a159d90cd1b051f
Ece Buse Bozkurt <250229045@kocaeli.edut.tr>
2026-05-18 16:07:36 +0300

    level sistemi ve kamera sinirlari gelistirildi.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ float velocityY=0.f;
 float gravity=0.5f;
 float jumpPower=-12.f;
 bool isOnGround=false;
-sf::RectangleShape ground({800.f,50.f});
+//oyun zemini
+sf::RectangleShape ground({3000.f,50.f});
 ground.setFillColor(sf::Color::Blue);
 ground.setPosition({0.f,550.f});
 std::vector<sf::RectangleShape>platforms;
 platforms.push_back(ground);
+
+//platformlar
 sf::RectangleShape platform1({30.f,100.f});
 platform1.setFillColor(sf::Color::Red);
 platform1.setPosition({250.f,400.f});
@@ -21,31 +24,49 @@ platforms.push_back(platform1);
 
 sf::RectangleShape platform2({30.f,100.f});
 platform2.setFillColor(sf::Color::Magenta);
-platform2.setPosition({500.f,300.f});
+platform2.setPosition({500.f,430.f});
 platforms.push_back(platform2);
+
+sf::RectangleShape platform3({200.f,30.f});
+platform3.setFillColor(sf::Color::Cyan);
+platform3.setPosition({900.f,450.f});
+platforms.push_back(platform3);
+
+sf::RectangleShape platform4({150.f,30.f});
+platform4.setFillColor(sf::Color::White);
+platform4.setPosition({1250.f,350.f});
+platforms.push_back(platform4);
+
+sf::RectangleShape platform5({250.f,30.f});
+platform5.setFillColor(sf::Color::Red);
+platform5.setPosition({1500.f,400.f});
+platforms.push_back(platform5);
 
 //oyuncu
   sf::RectangleShape player({50.f,70.f});
   player.setFillColor(sf::Color::Green);
   player.setPosition({100.f,50.f});
+
   //kamera olusturma
   sf::View view(sf::FloatRect({0.f,0.f},{800.f,600.f}));
+
   //coin
-  std::vector<sf::CircleShape>Coins;
+  std::vector<sf::CircleShape>coins;
   sf::CircleShape coin1(15.f);
   coin1.setFillColor(sf::Color::Yellow);
   coin1.setPosition({560.f,260.f});
-  Coins.push_back(coin1);
+  coins.push_back(coin1);
+
   sf::CircleShape coin2(15.f);
   coin2.setFillColor(sf::Color::Yellow);
   coin2.setPosition({300.f,360.f});
-  Coins.push_back(coin2);
-  bool coinCollected=false;
+  coins.push_back(coin2);
 
   sf::CircleShape coin3(15.f);
   coin3.setFillColor(sf::Color::Yellow);
   coin3.setPosition({120.f,510.f});
-  Coins.push_back(coin3);
+  coins.push_back(coin3);
+  //score ayari
   int score=0;
   sf::Font font;
   if(!font.openFromFile("/System/Library/Fonts/Supplemental/Arial.ttf"))
@@ -83,15 +104,31 @@ isOnGround=false;
 }
 velocityY+=gravity;
 player.move({0.f,velocityY});
-view.setCenter({player.getPosition().x+ player.getSize().x/2,300.f});
+if(player.getPosition().y>700.f)
+{
+player.setPosition({100.f,50.f});
+velocityY=0.f;
+score=0;
+}
+
+float cameraX=player.getPosition().x+player.getSize().x/2;
+if(cameraX<400.f)
+{
+cameraX=400.f;
+}
+if(cameraX>3000.f-400.f)
+{
+cameraX=3000.f-400.f;
+}
+view.setCenter({cameraX,300.f});
 window.setView(view);
 if(player.getPosition().x<0)
 {
 player.setPosition({0.f,player.getPosition().y});
 }
-if(player.getPosition().x+player.getSize().x>800)
+if(player.getPosition().x+player.getSize().x>3000)
 {
- player.setPosition({800.f-player.getSize().x,player.getPosition().y});
+ player.setPosition({3000.f-player.getSize().x,player.getPosition().y});
 }
 
 isOnGround=false;
@@ -106,11 +143,11 @@ velocityY=0.f;
 isOnGround=true;
 }
 }
-for(auto it=Coins.begin();it!=Coins.end();)
+for(auto it=coins.begin();it!=coins.end();)
 {
 if(player.getGlobalBounds().findIntersection(it->getGlobalBounds()))
 {
- it=Coins.erase(it);
+ it=coins.erase(it);
  score++;
 }
 else{
@@ -128,7 +165,7 @@ for(auto&platform:platforms)
 {
  window.draw(platform);
 }
-for(auto&coin:Coins)
+for(auto&coin:coins)
 {
 window.draw(coin);
 }
